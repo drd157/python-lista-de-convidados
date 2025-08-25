f8441f9b8064f12ffc8a4e4f01418dc1860a8dc0
drd <eduardo.ferreira.silva05@escola.pr.gov.br>
2025-08-25 09:41:06 -0300

    lista criada

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 # Lista de Convidados para a festa
+
+Convidados_da_festa = ['Nayelli','Igor','gabriel','Bianca','Daniela','Breno']
+
+#Dicíonario vazio para armazenar o status de precença de cada pessoa
+status_presenca = {}
+
+#Esta é a lista de pessoas que tentaram entrar na festa 
+lista_de_chegadas = ['igor','Grabriel','Bianca','Daniela']
